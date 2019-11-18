3bc6901a2480f7b156a5627634b37743cf874b42
mohitchandarana <chandarana.m@husky.neu.edu>
2019-11-18 15:33:07 -0500

    JavaDocs for For.java

--- src/main/java/com/codio/feature_usage_mod/controller/features/constructs/For.java
+++ src/main/java/com/codio/feature_usage_mod/controller/features/constructs/For.java
@@ -5,16 +5,34 @@ import com.github.javaparser.ast.stmt.ForStmt;
 
 import java.util.List;
 
+/**
+ * Java class to find For loops in Student Code.
+ */
+
 public class For {
 
   public For() {
   }
+
+  /**
+   * Method that finds all instances of For Loops in Student Code and maintains their count.
+   *
+   * @param cu AST object generated from the Student Code file.
+   * @return returns a message to the controller about instances of For Loops, as String.
+   */
 
   public String process(CompilationUnit cu) {
     List<ForStmt> forStmts = cu.findAll(ForStmt.class);
     int count = forStmts.size();
     return generateMessage(count);
   }
+
+  /**
+   * Private method that generates the message to be returned to the controller.
+   *
+   * @param count number of instances of For Loops found in Student Code.
+   * @return returns the message to be passed on to the controller, as String.
+   */
 
   private String generateMessage(int count) {
     if (count == 0) {
