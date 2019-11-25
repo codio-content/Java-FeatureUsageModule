a0ab9f230f75d8bbd1f1eebd0786adee24ddad03
mohitchandarana <chandarana.m@husky.neu.edu>
2019-11-25 13:46:11 -0500

    Finised Javadocs for Strings

--- src/main/java/com/codio/feature_usage_mod/controller/features/constructs/Strings.java
+++ src/main/java/com/codio/feature_usage_mod/controller/features/constructs/Strings.java
@@ -5,16 +5,34 @@ import com.github.javaparser.ast.expr.StringLiteralExpr;
 
 import java.util.List;
 
+/**
+ * Java class to find String objects in Student Code.
+ */
+
 public class Strings {
 
   public Strings() {
   }
+
+  /**
+   * Method that finds all instances of String objects in Student Code and maintains their count.
+   *
+   * @param cu AST object generated from Student Code file.
+   * @return message to the controller, as String.
+   */
 
   public String process(CompilationUnit cu) {
     List<StringLiteralExpr> strings = cu.findAll(StringLiteralExpr.class);
     int count = strings.size();
     return generateMessage(count);
   }
+
+  /**
+   * Private method that generates the message to be returned to the controller.
+   *
+   * @param count number of instances of String objects found in Student Code.
+   * @return message to be passed on to the controller, as String.
+   */
 
   private String generateMessage(int count) {
     if (count == 0) {
